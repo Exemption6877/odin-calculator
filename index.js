8fecfcb7a4efc2f1b7e1e09e4755b4f2291d208c
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-03 00:22:00 +0300

    Make + operator functional

--- index.js
+++ index.js
@@ -3,25 +3,43 @@ const operators = document.querySelectorAll(".operators");
 const equality = document.querySelector(".equality");
 
 let currentNumber = {
-  num: "",
+  first_num: "",
+  second_num: "",
   operator: null,
   compute: false,
 };
 
 numbers.forEach((button) => {
   button.addEventListener("click", () => {
-    currentNumber.num += [parseInt(button.innerText)];
-    console.log(currentNumber.num);
+    currentNumber.first_num += button.innerText;
+    console.log(currentNumber.first_num);
   });
 });
 
 operators.forEach((button) => {
   button.addEventListener("click", () => {
     currentNumber.operator = button.innerText;
+    currentNumber.second_num = currentNumber.first_num;
+    currentNumber.first_num = "";
     console.log(currentNumber.operator);
   });
 });
 
 equality.addEventListener("click", () => {
-  compute = true;
+  if (currentNumber.second_num.length > 0 && currentNumber.operator !== null) {
+    let num1 = parseInt(currentNumber.first_num);
+    let num2 = parseInt(currentNumber.second_num);
+
+    // reformat to switch
+    if (currentNumber.operator == "+") {
+      addCalculator(num1, num2);
+    }
+  } else {
+    console.log("123");
+  }
 });
+
+function addCalculator(x, y) {
+  console.log(x + y);
+  return x + y;
+}
